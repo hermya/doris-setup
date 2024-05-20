1cc67f5d944e476c6f1c53ddf5feece7ef4eb467
hermya <herambku@gmail.com>
2024-05-20 00:52:07 -0700

    Added shell file to download doris stable all-in-one binaries

---
+++ doris-setup/download_doris.sh
@@ -0,0 +1,6 @@
+wget https://apache-doris-releases.oss-accelerate.aliyuncs.com/apache-doris-2.0.10-bin-x64.tar.gz --no-check-certificate
+mkdir -p binaries
+mv apache-doris-2.0.10-bin-x64.tar.gz res/apache-doris-2.0.10-bin-x64.tar.gz
+cd binaries
+tar -xvzf apache-doris-2.0.10-bin-x64.tar.gz
+cd ..
