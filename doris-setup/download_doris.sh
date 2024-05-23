ce5b26f77d74e401793df5d171be536866e49a3d
hermya <herambku@gmail.com>
2024-05-23 03:23:10 -0700

    Added cmd to remove tar file after untarring

--- doris-setup/download_doris.sh
+++ doris-setup/download_doris.sh
@@ -3,4 +3,5 @@ mkdir -p binaries
 mv apache-doris-2.0.10-bin-x64.tar.gz res/apache-doris-2.0.10-bin-x64.tar.gz
 cd binaries
 tar -xvzf apache-doris-2.0.10-bin-x64.tar.gz
+rm apache-doris-2.0.10-bin-x64.tar.gz
 cd ..
